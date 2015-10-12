14aeba8c5014f6f9e6d6e4b54ac72100d97dbf13
Abdullah <abdullah.khan.niazi@hotmail.com>
2015-10-12 17:13:55 +0500

    initial commit

---
+++ build/templates-app.js
@@ -0,0 +1,8 @@
+angular.module('templates-app', ['home/home.tpl.html']);
+
+angular.module("home/home.tpl.html", []).run(["$templateCache", function($templateCache) {
+  $templateCache.put("home/home.tpl.html",
+    "<div>\n" +
+    "  welcome to home page\n" +
+    "</div>");
+}]);
