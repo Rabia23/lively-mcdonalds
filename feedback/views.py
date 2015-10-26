702ad7ae57b497abd4a51d06074c2aa4b62e80c8
rabia <rabia.iftikhar@arbisoft.com>
2015-10-26 11:28:02 +0500

    Change feedback query in feedback.views

--- feedback/views.py
+++ feedback/views.py
@@ -13,11 +13,29 @@ import json
 
 @api_view(['GET', 'POST'])
 def feedback_scores(request):
+
+    region_id = request.query_params.get('region', None)
+    city_id = request.query_params.get('city', None)
+    branch_id = request.query_params.get('branch', None)
+
     if request.method == 'GET':
 
-        scores = Feedback.objects.filter(branch__exact=1, branch__city__exact=1, branch__city__region__exact=1).\
-            values('score').\
-            annotate(count=Count('score'))
+        if region_id and city_id and branch_id:
+            scores = Feedback.objects.filter(branch__exact=branch_id, branch__city__exact=city_id, branch__city__region__exact=region_id).\
+                values('score').\
+                annotate(count=Count('score'))
+
+        elif region_id and city_id:
+            scores = Feedback.objects.filter(branch__city__exact=city_id, branch__city__region__exact=region_id).\
+                values('score').\
+                annotate(count=Count('score'))
+
+        elif region_id:
+            scores = Feedback.objects.filter(branch__city__region__exact=region_id).\
+                values('score').\
+                annotate(count=Count('score'))
+        else:
+            scores = Feedback.objects.values('score').annotate(count=Count('score'))
 
         total_scores = Feedback.objects.count()
         data = {'total_count': total_scores, 'scores': list(scores)}
