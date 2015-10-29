5e7ffe825d5b1c978e7e153fba2502d5ce88f178
aamish <aamish.iftikhar@arbisoft.com>
2015-10-29 20:01:33 +0500

    admin models added

--- feedback/admin.py
+++ feedback/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
-from feedback.models import Feedback
+from feedback.models import Feedback, Question, Option
 
 
 class FeedbackAdmin(admin.ModelAdmin):
     pass
 
 
+class QuestionAdmin(admin.ModelAdmin):
+    pass
+
+
+class OptionAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(Feedback, FeedbackAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Option, OptionAdmin)
