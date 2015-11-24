35dcdbdd8abf05b7672de4c38b93d755aa1a7560
aamish <aamish.iftikhar@arbisoft.com>
2015-11-24 11:43:05 +0500

    parse.com data migration fixes

--- app/management/commands/db_populate_user_info.py
+++ app/management/commands/db_populate_user_info.py
@@ -16,23 +16,27 @@ class Command(BaseCommand):
         self.stdout.write('Populating User Info Table')
         parse_user = Object.factory("User")
 
-        all_users = parse_user.Query.all().limit(10000)
-        for user in all_users:
-            first_name = user.first_name if hasattr(user, 'first_name') else ''
-            last_name = user.last_name if hasattr(user, 'last_name') else ''
-            phone_no = user.phone_no if hasattr(user, 'phone_no') else ''
-            is_customer = user.is_customer if hasattr(user, 'is_customer') else True
-            username = utils.generate_username()
-            self.stdout.write('objectId: ' + user.objectId + 'First Name: ' + first_name + 'Last Name : ' + last_name +
-                              ' Phone No: ' + phone_no)
+        all_users = parse_user.Query.all().limit(1000)
+        while True:
+            for user in all_users:
+                first_name = user.first_name if hasattr(user, 'first_name') else ''
+                last_name = user.last_name if hasattr(user, 'last_name') else ''
+                phone_no = user.phone_no if hasattr(user, 'phone_no') else ''
+                is_customer = user.is_customer if hasattr(user, 'is_customer') else True
+                username = utils.generate_username()
+                self.stdout.write('objectId: ' + user.objectId + 'First Name: ' + first_name + 'Last Name : ' + last_name +
+                                  ' Phone No: ' + phone_no)
 
-            django_user = User(first_name=first_name, last_name=last_name, username=username)
-            django_user.set_password("1234")
-            django_user.save()
+                django_user = User(first_name=first_name, last_name=last_name, username=username)
+                django_user.set_password("1234")
+                django_user.save()
 
-            django_user = User.objects.get(username=username)
-            local_user_info = UserInfo(user=django_user, phone_no=phone_no, objectId=user.objectId,
-                                       is_customer=is_customer)
-            local_user_info.save()
+                django_user = User.objects.get(username=username)
+                local_user_info = UserInfo(user=django_user, phone_no=phone_no, objectId=user.objectId,
+                                           is_customer=is_customer)
+                local_user_info.save()
+            all_users = parse_user.Query.all().limit(1000)
+            if all_users.count() <= 1000:
+                break
 
         self.stdout.write('Successfully Populated User and User Info Table')
