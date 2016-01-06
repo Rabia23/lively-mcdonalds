1a4117a6e8f6df5954ca00e504b6626f35aef266
aamish <aamish.iftikhar@arbisoft.com>
2016-01-06 17:25:07 +0500

    Websocket indentation fixing

--- apps/websocket.py
+++ apps/websocket.py
@@ -3,23 +3,19 @@ __author__ = 'aamish'
 import asyncio
 import websockets
 from apps.redis_queue import RedisQueue
-from lively import settings
 import random
+from lively import settings
+
 
 @asyncio.coroutine
 def ping(websocket, path):
     q = RedisQueue('feedback_queue')
+    print("Connection Opened")
     length = 0
-    print("length:" + str(length))
-    if websocket.open:
-        print("Opened")
-
     while True:
         if websocket.open:
             if length < q.qsize():
-                print("Seek")
                 length = q.qsize()
-                print("length:" + str(length))
                 print("Ping Received")
                 yield from websocket.send("Ping Received")
                 yield from asyncio.sleep(random.random() * 3)
